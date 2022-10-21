27877695af5ef537de796228fe309c5fabd252ac
RaymartBartilet <114345498+Raymartbartilet@users.noreply.github.com>
2022-10-21 21:49:44 +0800

    Add files via upload

---
+++ Unitsoflengths.py
@@ -0,0 +1,9 @@
+name = input("\n Enter your Name: ")
+print (" Hello, Good day " + name + '!\n')
+ft = int(input(" Input distance in feet:"))
+inches = ft * 12
+yards = ft / 3.0
+miles = ft / 5280.0
+print("\n The distance in inches is %i inches." % inches)
+print(" The distance in yards is %.2f yards." % yards)
+print(" The distance in miles is %.2f miles." % miles)
